ec21bdeaf4153d39af6353e4aa6e5fed475541ff
Rodrigo Oliveira <rodrigo.oliveira@xgen.com.br>
2022-02-01 15:26:02 -0300

    added church unit edit page

--- src/app/modules/settings/components/church-unit-edit/church-unit-edit.component.ts
+++ src/app/modules/settings/components/church-unit-edit/church-unit-edit.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./church-unit-edit.component.scss']
 })
 export class ChurchUnitEditComponent implements OnInit {
+  churchUnitForm: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  onSubmit() {}
+
+  onResetForm() {
+    this.churchUnitForm.reset();
+  }
+
+  private initForm() {
+    let initName = '';
+    let initUnit = '';
+    let initUnitType = '';
+
+    this.churchUnitForm = new FormGroup({
+      name: new FormControl(initName, Validators.required),
+      unitId: new FormControl(initUnit, Validators.required),
+      unitType: new FormControl(initUnitType, Validators.required)
+    })
+  }
 }
