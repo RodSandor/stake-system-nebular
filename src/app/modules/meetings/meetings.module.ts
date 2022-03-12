b8ce43772308d7a3cac75c917c74cb86c7c02dbc
Rodrigo Sandor <rod.sandor@gmail.com>
2022-03-11 22:50:08 -0300

    removed event component

--- src/app/modules/meetings/meetings.module.ts
+++ src/app/modules/meetings/meetings.module.ts
@@ -1,22 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { AgendaTopicEditComponent } from './components/agenda-topic-edit/agenda-topic-edit.component';
+import { AgendaEventEditComponent } from './components/agenda-event-edit/agenda-event-edit.component';
 import { MeetingListPageComponent } from './pages/meeting-list-page/meeting-list-page.component';
 import { AgendaEditComponent } from './components/agenda-edit/agenda-edit.component';
 import { MeetingsRoutingModule } from './meetings-routing.module';
 import { SharedModule } from './../../shared/shared.module';
 import { CoreModule } from './../../core/core.module';
-import { AgendaTopicEditComponent } from './components/agenda-topic-edit/agenda-topic-edit.component';
-import { AgendaEventComponent } from './components/agenda-event/agenda-event.component';
-import { AgendaEventEditComponent } from './components/agenda-event-edit/agenda-event-edit.component';
-
 
 @NgModule({
   declarations: [
     AgendaEditComponent,
     MeetingListPageComponent,
     AgendaTopicEditComponent,
-    AgendaEventComponent,
     AgendaEventEditComponent
   ],
   imports: [
